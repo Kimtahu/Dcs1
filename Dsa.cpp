45692ed8cd5d7f81693db41b4f6aad2bdb9ade9b
Kimtahu <105015112+Kimtahu@users.noreply.github.com>
2022-05-05 12:31:12 -0700

    Add files via upload

---
+++ Dsa.cpp
@@ -0,0 +1,17 @@
+// 1.Arithmetic Operation 
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a,b;
+     cout<<"Enter the value of a :- ";
+     cin>>a;
+     cout<<"Enter the value of b :- ";
+     cin>>b;
+
+     cout<<"a + b = "<<a+b<<endl;
+     cout<<"a - b = "<<a-b<<endl;
+     cout<<"a * b = "<<a*b<<endl;
+     cout<<"a / b = "<<a/b<<endl;
+     return 0;
+}
