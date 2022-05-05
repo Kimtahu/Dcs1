a712caf4bbead450ac70f83d7c86adb7df74dac3
Kimtahu <105015112+Kimtahu@users.noreply.github.com>
2022-05-05 13:22:28 -0700

    Dsa3
    3Code

---
+++ Dsa3.cpp
@@ -0,0 +1,12 @@
+// 3.Area of square
+#include <iostream>
+using namespace std;
+int main()
+{
+     int s;
+     cout << "Enter the value of s :- ";
+     cin >> s;
+
+     cout << "Area of square is :- " << s * s << endl;
+     return 0;
+}
